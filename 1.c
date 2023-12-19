ef26546013d720bc713ee79507b36b74a2050b13
deepak <109057450+deepak6969@users.noreply.github.com>
2023-12-19 14:30:57 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -3,241 +3,96 @@
 
 struct Node {
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node* prev;
+    struct Node* next;
 };
 
-// Function to create a new node
-struct Node* createNode(int data) {
+struct Node* head = NULL;
+
+void insertElement(int element) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) {
-        printf("Memory allocation failed!");
-        exit(1);
+    newNode->data = element;
+
+    if (head == NULL) {
+        head = newNode;
+        head->prev = head;
+        head->next = head;
+    } else {
+        struct Node* last = head->prev;
+        newNode->next = head;
+        newNode->prev = last;
+        head->prev = newNode;
+        last->next = newNode;
     }
-    newNode->data = data;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+
+    printf("Element %d inserted successfully.\n", element);
 }
 
-// Function for iterative in-order traversal
-void iterativeInOrder(struct Node* root) {
-    if (root == NULL)
+void deleteElement(int element) {
+    if (head == NULL) {
+        printf("List is empty. Cannot delete element.\n");
         return;
-
-    struct Node* stack[100];
-    int top = -1;
-    struct Node* current = root;
-
-    while (current != NULL || top != -1) {
-        while (current != NULL) {
-            stack[++top] = current;
-            current = current->left;
-        }
-
-        current = stack[top--];
-        printf("%d ", current->data);
-        current = current->right;
-    }
-}
-
-// Function for iterative post-order traversal
-void iterativePostOrder(struct Node* root) {
-    if (root == NULL)
-        return;
-
-    struct Node* stack1[100];
-    struct Node* stack2[100];
-    int top1 = -1;
-    int top2 = -1;
-
-    stack1[++top1] = root;
-
-    while (top1 != -1) {
-        struct Node* temp = stack1[top1--];
-        stack2[++top2] = temp;
-
-        if (temp->left != NULL)
-            stack1[++top1] = temp->left;
-        if (temp->right != NULL)
-            stack1[++top1] = temp->right;
     }
 
-    while (top2 != -1) {
-        struct Node* temp = stack2[top2--];
-        printf("%d ", temp->data);
-    }
-}
+    struct Node* current = head;
+    struct Node* prev = NULL;
 
-// Function for iterative pre-order traversal
-void iterativePreOrder(struct Node* root) {
-    if (root == NULL)
-        return;
-
-    struct Node* stack[100];
-    int top = -1;
-
-    stack[++top] = root;
-
-    while (top != -1) {
-        struct Node* temp = stack[top--];
-        printf("%d ", temp->data);
-
-        if (temp->right != NULL)
-            stack[++top] = temp->right;
-        if (temp->left != NULL)
-            stack[++top] = temp->left;
-    }
-}
-
-// Function to print the parent of a given element
-void printParent(struct Node* root, int element) {
-    if (root == NULL)
-        return;
-
-    struct Node* stack[100];
-    int top = -1;
-
-    stack[++top] = root;
-
-    while (top != -1) {
-        struct Node* temp = stack[top--];
-
-        if ((temp->left != NULL && temp->left->data == element) || (temp->right != NULL && temp->right->data == element)) {
-            printf("Parent of %d is %d\n", element, temp->data);
+    while (current->data != element) {
+        if (current->next == head) {
+            printf("Element %d not found in the list.\n", element);
             return;
         }
 
-        if (temp->right != NULL)
-            stack[++top] = temp->right;
-        if (temp->left != NULL)
-            stack[++top] = temp->left;
+        prev = current;
+        current = current->next;
     }
 
-    printf("Element not found in the tree!\n");
-}
-
-// Function to print the depth of the tree
-int getDepth(struct Node* root) {
-    if (root == NULL)
-        return 0;
-
-    int depth = 0;
-    struct Node* queue[100];
-    int front = -1;
-    int rear = -1;
-
-    queue[++rear] = root;
-
-    while (front != rear) {
-        int size = rear - front;
-
-        while (size--) {
-            struct Node* temp = queue[++front];
-
-            if (temp->left != NULL)
-                queue[++rear] = temp->left;
-            if (temp->right != NULL)
-                queue[++rear] = temp->right;
-        }
-
-        depth++;
+    if (current == head && current->next == head) {
+        head = NULL;
+    } else if (current == head) {
+        prev = head->prev;
+        head = head->next;
+        prev->next = head;
+        head->prev = prev;
+    } else {
+        prev->next = current->next;
+        current->next->prev = prev;
     }
 
-    return depth;
-}
-
-// Function to print the ancestors of a given element
-void printAncestors(struct Node* root, int element) {
-    if (root == NULL)
-        return;
-
-    struct Node* stack[100];
-    int top = -1;
-
-    stack[++top] = root;
-
-    while (top != -1) {
-        struct Node* temp = stack[top--];
-
-        if (temp->data == element) {
-            printf("Ancestors of %d are: ", element);
-            while (top != -1) {
-                printf("%d ", stack[top--]->data);
-            }
-            printf("\n");
-            return;
-        }
-
-        if (temp->right != NULL)
-            stack[++top] = temp->right;
-        if (temp->left != NULL)
-            stack[++top] = temp->left;
-    }
-
-    printf("Element not found in the tree!\n");
-}
-
-// Function to count the number of leaf nodes in a binary tree
-int countLeafNodes(struct Node* root) {
-    if (root == NULL)
-        return 0;
-
-    int count = 0;
-    struct Node* stack[100];
-    int top = -1;
-
-    stack[++top] = root;
-
-    while (top != -1) {
-        struct Node* temp = stack[top--];
-
-        if (temp->left == NULL && temp->right == NULL)
-            count++;
-
-        if (temp->right != NULL)
-            stack[++top] = temp->right;
-        if (temp->left != NULL)
-            stack[++top] = temp->left;
-    }
-
-    return count;
+    free(current);
+    printf("Element %d deleted successfully.\n", element);
 }
 
 int main() {
-    // Create a binary tree
-    struct Node* root = createNode(1);
-    root->left = createNode(2);
-    root->right = createNode(3);
-    root->left->left = createNode(4);
-    root->left->right = createNode(5);
-    root->right->left = createNode(6);
-    root->right->right = createNode(7);
+    int choice, element;
 
-    // Perform operations on the binary tree
-    printf("In-order traversal: ");
-    iterativeInOrder(root);
-    printf("\n");
+    do {
+        printf("\nMenu:\n");
+        printf("1. Insert an element\n");
+        printf("2. Delete an element\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    printf("Post-order traversal: ");
-    iterativePostOrder(root);
-    printf("\n");
-
-    printf("Pre-order traversal: ");
-    iterativePreOrder(root);
-    printf("\n");
-
-    int element = 5;
-    printParent(root, element);
-
-    int depth = getDepth(root);
-    printf("Depth of the tree: %d\n", depth);
-
-    element = 7;
-    printAncestors(root, element);
-
-    int leafCount = countLeafNodes(root);
-    printf("Number of leaf nodes: %d\n", leafCount);
+        switch (choice) {
+            case 1:
+                printf("Enter the element to insert: ");
+                scanf("%d", &element);
+                insertElement(element);
+                break;
+            case 2:
+                printf("Enter the element to delete: ");
+                scanf("%d", &element);
+                deleteElement(element);
+                break;
+            case 3:
+                printf("Exiting program.\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 3);
 
     return 0;
 }
+
--- 2.c
+++ 2.c
@@ -1,58 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a binary tree node
-struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
+// Structure to represent a term in a polynomial
+struct Term {
+    int coefficient;
+    int exponent;
+    struct Term* next;
 };
 
-// Function to create a new node
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+// Function to create a new term
+struct Term* createTerm(int coefficient, int exponent) {
+    struct Term* newTerm = (struct Term*)malloc(sizeof(struct Term));
+    newTerm->coefficient = coefficient;
+    newTerm->exponent = exponent;
+    newTerm->next = NULL;
+    return newTerm;
 }
 
-// Function to insert a node in the binary tree
-struct Node* insertNode(struct Node* root, int data) {
-    if (root == NULL) {
-        root = createNode(data);
-    } else {
-        if (data <= root->data) {
-            root->left = insertNode(root->left, data);
+// Function to add two polynomials
+struct Term* addPolynomials(struct Term* poly1, struct Term* poly2) {
+    struct Term* result = NULL;
+    struct Term* current = NULL;
+
+    while (poly1 != NULL && poly2 != NULL) {
+        struct Term* newTerm = NULL;
+
+        if (poly1->exponent > poly2->exponent) {
+            newTerm = createTerm(poly1->coefficient, poly1->exponent);
+            poly1 = poly1->next;
+        } else if (poly1->exponent < poly2->exponent) {
+            newTerm = createTerm(poly2->coefficient, poly2->exponent);
+            poly2 = poly2->next;
         } else {
-            root->right = insertNode(root->right, data);
+            int sum = poly1->coefficient + poly2->coefficient;
+            if (sum != 0) {
+                newTerm = createTerm(sum, poly1->exponent);
+            }
+            poly1 = poly1->next;
+            poly2 = poly2->next;
+        }
+
+        if (newTerm != NULL) {
+            if (result == NULL) {
+                result = newTerm;
+                current = newTerm;
+            } else {
+                current->next = newTerm;
+                current = current->next;
+            }
         }
     }
-    return root;
+
+    while (poly1 != NULL) {
+        struct Term* newTerm = createTerm(poly1->coefficient, poly1->exponent);
+        poly1 = poly1->next;
+
+        if (result == NULL) {
+            result = newTerm;
+            current = newTerm;
+        } else {
+            current->next = newTerm;
+            current = current->next;
+        }
+    }
+
+    while (poly2 != NULL) {
+        struct Term* newTerm = createTerm(poly2->coefficient, poly2->exponent);
+        poly2 = poly2->next;
+
+        if (result == NULL) {
+            result = newTerm;
+            current = newTerm;
+        } else {
+            current->next = newTerm;
+            current = current->next;
+        }
+    }
+
+    return result;
 }
 
-// Function to print the binary tree in inorder traversal
-void printInorder(struct Node* root) {
-    if (root != NULL) {
-        printInorder(root->left);
-        printf("%d ", root->data);
-        printInorder(root->right);
+// Function to display a polynomial
+void displayPolynomial(struct Term* poly) {
+    if (poly == NULL) {
+        printf("Polynomial is empty.\n");
+        return;
     }
+
+    while (poly != NULL) {
+        printf("%dx^%d ", poly->coefficient, poly->exponent);
+        poly = poly->next;
+        if (poly != NULL) {
+            printf("+ ");
+        }
+    }
+    printf("\n");
 }
 
 int main() {
-    struct Node* root = NULL;
-    root = insertNode(root, 5);
-    root = insertNode(root, 3);
-    root = insertNode(root, 7);
-    root = insertNode(root, 2);
-    root = insertNode(root, 4);
-    root = insertNode(root, 6);
-    root = insertNode(root, 8);
+    // Create the first polynomial: 3x^2 + 2x + 1
+    struct Term* poly1 = createTerm(3, 2);
+    poly1->next = createTerm(2, 1);
+    poly1->next->next = createTerm(1, 0);
 
-    printf("Inorder traversal of the binary tree: ");
-    printInorder(root);
+    // Create the second polynomial: 2x^3 + 4x^2 + 1
+    struct Term* poly2 = createTerm(2, 3);
+    poly2->next = createTerm(4, 2);
+    poly2->next->next = createTerm(1, 0);
+
+    // Add the polynomials
+    struct Term* result = addPolynomials(poly1, poly2);
+
+    // Display the result
+    printf("Result: ");
+    displayPolynomial(result);
 
     return 0;
 }
-
